314a2c0381a4d12e8c647ffc4c327364da0fb3e6
Kleiner Hacker <Christoph.Pfeiffer.Berlin@freenet.de>
2017-12-23 20:45:41 +0100

    insert commenter

---
+++ src/org/pcsoft/plugins/intellij/iss/language/common/IssCommenter.java
@@ -0,0 +1,37 @@
+package org.pcsoft.plugins.intellij.iss.language.common;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class IssCommenter implements Commenter {
+
+    @Nullable
+    @Override
+    public String getLineCommentPrefix() {
+        return ";";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentSuffix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
