a86616ef10a26ae908cab33f57a287392df461c4
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2026-05-31 22:24:43 +0200

    rebuild

--- src/org/pcsoft/plugins/intellij/iss/language/parser/manipulator/IssRefValueManipulator.java
+++
@@ -1,6 +0,0 @@
-package org.pcsoft.plugins.intellij.iss.language.parser.manipulator;
-
-import org.pcsoft.plugins.intellij.iss.language.parser.psi.element.IssRefValue;
-
-public class IssRefValueManipulator extends IssAbstractElementManipulator<IssRefValue> {
-}
