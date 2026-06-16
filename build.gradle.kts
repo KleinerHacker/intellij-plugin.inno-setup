9954273771d772bc16fb6aed6e4e4d28308592f4
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2026-06-16 15:10:19 +0200

    insert SBOM support

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ plugins {
     id("org.jetbrains.dokka") version "2.2.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.1"
     id("com.github.jk1.dependency-license-report") version "2.5"
+    id("org.cyclonedx.bom") version "3.2.4"
     id("app.cash.licensee") version "1.14.1"
 }
 
